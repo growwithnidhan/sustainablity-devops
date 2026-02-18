bc4de5984dda23f8e971ce1294b7c3de827cc653
growwithnidhan <mail2nidhan@gmail.com>
2026-02-18 13:05:36 +0530

    Initial commit - Python app with pytest setup

---
+++ main.py
@@ -0,0 +1,5 @@
+from app.addition import add
+from app.subtraction import subtract
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
---
+++ tests/test_subtraction.py
@@ -0,0 +1,4 @@
+from app.subtraction import subtract
+
+def test_subtract():
+    assert subtract(5, 3) == 2
